172638c25a2d30c205690b875ea6a6f24cb0452e
andreinwald <andreinwald@gmail.com>
2023-12-10 20:22:31 +0100

    Added MX domains DNS cache

--- test/index.test.js
+++ test/index.test.js
@@ -5,7 +5,7 @@ let emails = {
     'someone@gmail.com': true,
     'som_e-one@gmail.com': false,
     'someone@yahoo.com': true,
-    'som_e-+one@yahoo.com': false,
+    'some+one@yahoo.com': false,
 };
 for (let email in emails) {
     test(email, () => isValidEmail(email).then(result => expect(result).toBe(emails[email])));
